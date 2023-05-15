f275ed8c7c727165eaa8cbe0dce3c15ef10ba477
onbernard <bernard.onesime@gmail.com>
2023-05-16 00:55:48 +0200

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,76 @@
+<script lang='ts'>
+	import '../theme.postcss';
+	// The ordering of these imports is critical to your app working properly
+	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+	// Most of your app wide CSS should be put in this file
+	import '../app.postcss';
+	import { AppShell, AppBar, LightSwitch } from '@skeletonlabs/skeleton';
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-2xl">O. BERNARD</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a
+					class="social-btn"
+					href="https://fr.linkedin.com/in/onésime-bernard-82b953173"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<i class="fa-brands fa-linkedin-in"></i>
+				</a>
+				<a
+					class="social-btn"
+					href="https://twitter.com/emiseno"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<i class="fa-brands fa-twitter"></i>
+				</a>
+				<a
+					class="social-btn"
+					href="https://github.com/onbernard"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<i class="fa-lg fa-brands fa-github"></i>
+				</a>
+				<a
+					class="resume-btn"
+					href="resume_onesime_bernard.pdf"
+					download
+				>
+					<i class="fa-regular fa-file-lines"></i>
+					<span class="font-semibold">Get my resume</span>
+				</a>
+				<h2 class="gradient-heading">
+					<div class="bg-surface-50-900-token text-token text-xl font-semibold p-3">Let's connect!</div>
+				</h2>
+				<LightSwitch />
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
+
+<style lang="postcss">
+	.social-btn {
+		@apply btn btn-icon variant-ringed-surface;
+	}
+	.resume-btn {
+		@apply btn variant-ringed-surface;
+	}
+	.connect-btn {
+		@apply btn btn-xl variant-ringed rounded-none;
+	}
+	.gradient-heading {
+		@apply btn rounded-none bg-gradient-to-br variant-gradient-primary-tertiary p-0.5;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,51 @@
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+
+<div class="container mx-auto flex justify-center">
+	<div class="flex flex-col items-start">
+		<span class="text-4xl font-extrabold">Hi<i class="fa-regular fa-hand fa-shake"></i> I'm Onésime</span>
+		<p>I am a computer science and mathematics student at Université Paul Sabatier de Toulouse. I previously studied engineering at the École Polytechnique de Louvain in Belgium. Science fascinates me and I love to learn new stuff. My interests lies primarily with computer simulations and how to solve real-world problem with procedural solutions. To that end, i like to tinker with artificial intelligence and more conventional algorithms.
+
+		When I am not breaking a computer, you can find me on my bike 🚴, watching Formula 1 🏎️ or playing video games 🎮 with friends. I can be a massive nerd but i learned to live with it.
+		</p>
+		<span class="font-bold">Let's connect <i class="fa-solid fa-arrow-right"></i></span>
+	</div>
+</div>
+<section class="flex flex-row bg-gradient-to-br variant-gradient-primary-tertiary">
+	<i class="fa-10x text-surface-50-900-token fa-solid fa-link"></i>
+	<h2 class="h2 text-surface-50-900-token">Get in touch</h2>
+</section>
+
+<style lang="postcss">
+	figure {
+		@apply flex relative flex-col;
+	}
+	figure svg,
+	.img-bg {
+		@apply w-64 h-64 md:w-80 md:h-80;
+	}
+	.img-bg {
+		@apply absolute z-[-1] rounded-full blur-[50px] transition-all;
+		animation: pulse 5s cubic-bezier(0, 0, 0, 0.5) infinite,
+			glow 5s linear infinite;
+	}
+	@keyframes glow {
+		0% {
+			@apply bg-primary-400/50;
+		}
+		33% {
+			@apply bg-secondary-400/50;
+		}
+		66% {
+			@apply bg-tertiary-400/50;
+		}
+		100% {
+			@apply bg-primary-400/50;
+		}
+	}
+	@keyframes pulse {
+		50% {
+			transform: scale(1.5);
+		}
+	}
+
+</style>
